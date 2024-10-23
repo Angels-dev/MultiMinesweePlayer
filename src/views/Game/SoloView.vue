26c8b60f610c5f0aee6ada6f7015e614da29e778
Zachary Guénot <zachary@zac.ovh>
2024-10-24 00:46:19 +0200

    Création interface de base

---
+++ src/views/Game/SoloView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="main">
+    <h1>Solo</h1>
+  </div>
+</template>
---
+++ src/views/GameView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="sidenav">
+      <router-link to="/">Home</router-link>
+      <div class="vl"></div>
+    </div>
+
+    <router-view v-slot="{ Component }">
+      <transition name="slide-fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
+</template>

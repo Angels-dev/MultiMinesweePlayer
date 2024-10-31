ee3cbc0996aafc2dfb2975575ba84088c6e9af6f
Zachary Guénot <zachary@zac.ovh>
2024-10-31 20:52:17 +0100

    Vuetify init

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+// Components
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const vuetify = createVuetify({})
 
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+    .use(router)
+    .use(vuetify)
+    .mount('#app')
